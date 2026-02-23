8220281ea94b78b911a5ab7985b12644e3757572
abdulahad934 <adbulahad934@gmail.com>
2026-02-23 19:03:42 +0600

    url patterns and settings for the ZyraMart project, including CORS configuration and media file handling.

--- backend/ZyraMart/ZyraMart/urls.py
+++ backend/ZyraMart/ZyraMart/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/users/", include("users.urls")),
+    path("api/products/", include("products.urls")),
+    path("api/orders/", include("orders.urls")),
+    path("api/payments/", include("payments.urls")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
