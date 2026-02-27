0ea88b0c6f6cb1bf3ed8bb7d1aa67d41a5811983
abdulahad934 <adbulahad934@gmail.com>
2026-02-28 01:18:41 +0600

    added

--- backend/ZyraMart/ZyraMart/urls.py
+++ backend/ZyraMart/ZyraMart/urls.py
@@ -19,7 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+# from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework_simplejwt.views import TokenRefreshView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("users.urls")),
--- backend/ZyraMart/users/views.py
+++ backend/ZyraMart/users/views.py
@@ -1,29 +1,39 @@
-from rest_framework_simplejwt.tokens import RefreshToken
+# views.py
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from django.contrib.auth import authenticate
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import AdminLoginSerializer
 
-
-
-
-# Admin Login View api
 @api_view(['POST'])
 def admin_login(request):
-    username = request.data.get('username')
-    password = request.data.get('password')
-
-    user = authenticate(username = username, password = password)
-
-    if user is not None and user.is_staff:
-        refresh = RefreshToken.for_user(user)
+    serializer = AdminLoginSerializer(data=request.data)
+    
+    if not serializer.is_valid():
         return Response({
-            'message': 'Login successfull!',
-            'access': str(refresh.access_token),
-            'refresh': str(refresh)
-        }, status= status.HTTP_200_OK)
+            "success": False,
+            "message": "Validation error",
+            "errors": serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+    
+    username = serializer.validated_data['username']
+    password = serializer.validated_data['password']
+    
+    user = authenticate(username=username, password=password)
+    
+    if user is None or not user.is_staff:
+        return Response({
+            "success": False,
+            "message": "Invalid credentials or not an admin"
+        }, status=status.HTTP_401_UNAUTHORIZED)
+    
+    # Generate JWT tokens
+    refresh = RefreshToken.for_user(user)
     
     return Response({
-        'error': 'Invalid credentials or not an admin'
-    }, status=status.HTTP_401_UNAUTHORIZED)
+        "success": True,
+        "message": "Login successful",
+        "access": str(refresh.access_token),
+        "refresh": str(refresh)
+    }, status=status.HTTP_200_OK)
